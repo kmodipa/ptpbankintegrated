92a21ef5bf3d5871b70278b0d5d38ea59af6a84a
Khomotjo Modipa <khomotjomodipa4@gmail.com>
2026-05-06 13:46:59 +0200

    remove pages

--- src/PTPBank.Web/Pages/About.cshtml.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace PTPBank.Web.Pages;
-
-public class AboutModel : PageModel
-{
-    public void OnGet() { }
-}
--- src/PTPBank.Web/Pages/Contact.cshtml.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace PTPBank.Web.Pages;
-
-public class ContactModel : PageModel
-{
-    public void OnGet() { }
-}
